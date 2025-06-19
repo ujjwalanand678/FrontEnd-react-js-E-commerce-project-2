33364a08dbf41d99299e3c71b8fd78d37d099eea
Ujjwal Anand <ujjwalanand678@gmail.com>
2025-06-19 19:31:04 +0530

    redux

--- src/Components/ProductDetail.jsx
+++ src/Components/ProductDetail.jsx
@@ -2,26 +2,32 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import "./Products.css";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../redux/cartSlice";
 
 const ProductDetail = () => {
   const { id } = useParams();
+  const dispatch = useDispatch()
   const [product, setProduct] = useState({});
   useEffect(() => {
     getProductDetail();
   }, []);
+
   const getProductDetail = async () => {
     const { data } = await axios(
       `https://fakestoreapi.com/products/${id}`
     );
     setProduct(data);
   };
-  
+
+  const handleAddToCart= (prod)=>{
+    dispatch(addToCart(product))
+  }
   return (
     <>
       <div>
         <h1 className="product-detail">Product Details</h1>
-        {
-          <div className="cart-card">
+        {product && Object.keys(product).length > 0 ? ( <div className="cart-card">
             <div className="product-card-img">
               <img src={product.image} alt="" />
             </div>
@@ -31,9 +37,10 @@ const ProductDetail = () => {
               <span className="rating">Rating: {product.rating?.rate} </span> 
               <span className="price-1">Price: ${product.price} </span> <br /><br />
               {/* The optional chaining (?.) ensures your app doesn't crash before the data loads. */}
-              <button className="add-to-cart">Add to Cart</button>
+              <button className="add-to-cart" onClick={()=>handleAddToCart(product)}>Add to Cart</button>
             </div>
-          </div>
+          </div>) : <h2>Loading......!</h2>
+         
         }
       </div>
     </>
