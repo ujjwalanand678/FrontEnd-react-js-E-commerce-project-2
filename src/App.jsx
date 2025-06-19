50b5cf65ea4ba00352081b9b82fb10aa941588f0
Ujjwal Anand <ujjwalanand678@gmail.com>
2025-06-19 15:55:48 +0530

    c

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import Routing from "./Routes/Routing";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <>
       <Navbar />
       <Routing />
+      <Analytics/>
     </>
   );
 }
