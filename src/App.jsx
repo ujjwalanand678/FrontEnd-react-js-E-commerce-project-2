284499c11b80e95e941cf8033477f2df286dee73
Ujjwal Anand <ujjwalanand678@gmail.com>
2025-06-19 15:22:43 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
+import React, { Suspense, lazy } from "react";
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import Routing from "./Routes/Routing";
+const Analytics = React.lazy(() => import("@vercel/analytics/react"));
 
 function App() {
   return (
     <>
       <Navbar />
       <Routing />
+      <React.Suspense fallback={null}>
+        <Analytics />
+      </React.Suspense>
     </>
   );
 }
