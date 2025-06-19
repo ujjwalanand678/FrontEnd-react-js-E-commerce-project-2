a6fedd79fd624588a80b79f7c1f3e5a0e0aa6dd9
Ujjwal Anand <ujjwalanand678@gmail.com>
2025-06-19 14:30:26 +0530

    vercel added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import Routing from "./Routes/Routing";
+import { Analytics } from "@vercel/analytics/next"
 
 function App() {
   return (
     <>
       <Navbar/>
       <Routing/>
+        <Analytics />
     </>
   );
 }
