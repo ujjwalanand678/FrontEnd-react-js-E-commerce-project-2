7571a58650d0efffd58b171b560468f60b76cfeb
Ujjwal Anand <ujjwalanand678@gmail.com>
2025-06-19 18:40:35 +0530

    changes

--- src/Components/ProductDetail.jsx
+++ src/Components/ProductDetail.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import "./Products.css";
 
 const ProductDetail = () => {
   const { id } = useParams();
@@ -9,23 +10,31 @@ const ProductDetail = () => {
     getProductDetail();
   }, []);
   const getProductDetail = async () => {
-    const { data } = await axios(`https://fakestoreapi.com/products/${id}`);
+    const { data } = await axios(
+      `https://fakestoreapi.com/products/${id}`
+    );
     setProduct(data);
   };
+  
   return (
     <>
       <div>
         <h1 className="product-detail">Product Details</h1>
-        <div>
-          <div>
-            <h3>{product.title}</h3>
-            <img src={product.image} alt="" />
-            <p>{product.description}</p>
-            <p>Price :${product.price}</p>
-            {/* <p>Rating: {product.rating.rate}</p> */}
-            
+        {
+          <div className="cart-card">
+            <div className="product-card-img">
+              <img src={product.image} alt="" />
+            </div>
+            <div className="product-description">
+              <h3>{product.title}</h3>
+              <p>{product.description}</p>
+              <span className="rating">Rating: {product.rating?.rate} </span> 
+              <span className="price-1">Price: ${product.price} </span> <br /><br />
+              {/* The optional chaining (?.) ensures your app doesn't crash before the data loads. */}
+              <button className="add-to-cart">Add to Cart</button>
+            </div>
           </div>
-        </div>
+        }
       </div>
     </>
   );
