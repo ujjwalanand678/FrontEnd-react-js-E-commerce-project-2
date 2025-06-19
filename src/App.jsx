8b069fb3a56f954604072d07adb6b32916893d9c
Ujjwal Anand <ujjwalanand678@gmail.com>
2025-06-19 14:49:14 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import Routing from "./Routes/Routing";
-import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <>
       <Navbar/>
       <Routing/>
-      <Analytics />
+      
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react"
+
 
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <App />
+      <Analytics />
     </BrowserRouter>
   </StrictMode>
 );
