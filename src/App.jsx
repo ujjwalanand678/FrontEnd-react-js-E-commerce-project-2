ef7ae925a42edffd38e8848793e1841e2755e704
Ujjwal Anand <ujjwalanand678@gmail.com>
2025-06-22 18:42:04 +0530

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import Routing from "./Routes/Routing";
-import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <>
       <Navbar />
       <Routing />
-      <Analytics/>
     </>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   build: {
-    rollupOptions: {
-      external: ['@rollup/rollup-linux-x64-gnu']
-    }
-  },
-  base:"/FrontEnd-react-js-E-commerce-project-2",
 
-})
+  base: "/FrontEnd-react-js-E-commerce-project-2",
+});
