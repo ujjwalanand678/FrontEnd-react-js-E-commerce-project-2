0d0af20e135a5d8d92cb7fefa1f2ebd0b608e6aa
Ujjwal Anand <ujjwalanand678@gmail.com>
2025-06-22 18:00:23 +0530

    save

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   build: {
+    rollupOptions: {
+      external: ['@rollup/rollup-linux-x64-gnu']
+    }
+  },
   base:"/FrontEnd-react-js-E-commerce-project-2",
 
 })
